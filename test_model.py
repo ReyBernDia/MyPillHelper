6e77c297e31f60df9bed1c339802c22d704828ca
Reyna Diaz <rb.diaz2015@gmail.com>
2019-11-06 15:58:38 -0800

    Test DB functioning with test data and without images.

---
+++ test_model.py
@@ -0,0 +1,57 @@
+"""Models and database functions for MyPill project."""
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import asc, update
+
+# This is the connection to the PostgreSQL database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+
+##############################################################################
+# Model definitions
+
+
+class Meds(db.Model):
+    """All medications."""
+
+    __tablename__ = "meds"
+
+    med_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    shape = db.Column(db.String(20), nullable=True)
+    score = db.Column(db.Integer, nullable=True)
+    imprint = db.Column(db.String(25), nullable=True)
+    color = db.Column(db.String(20), nullable=True)
+    strength = db.Column(db.String(64), nullable=True)
+    rxcui = db.Column(db.String(15), nullable=True)
+    ndc9 = db.Column(db.String(20), nullable=False)
+    medicine_name = db.Column(db.String(64), nullable=True)
+    image_label = db.Column(db.String(64), nullable=True)
+    has_image = db.Column(db.Boolean, nullable=True)
+
+    def __repr__(self):
+        return f"<Medication: {self.medicine_name} RXCUI: {self.rxcui}>"
+
+##############################################################################
+# Helper functions
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///meds'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
---
+++ test_seed.py
@@ -0,0 +1,126 @@
+"""Utility file to seed image database from MovieLens data in seed_data/"""
+
+from sqlalchemy import func
+
+from test_model import Meds
+
+from test_model import connect_to_db, db
+from server import app
+from datetime import datetime
+import csv
+
+
+#EXAMPLE DATA PULL FROM SEED DATA.
+# def load_users():
+#     """Load users from u.user into database."""
+
+#     print("Users")
+
+#     # Delete all rows in table, so if we need to run this a second time,
+#     # we won't be trying to add duplicate users
+#     User.query.delete()
+
+#     # Read u.user file and insert data
+#     for row in open("seed_data/u.user"):
+#         row = row.rstrip()
+#         user_id, age, gender, occupation, zipcode = row.split("|")
+
+#         user = User(user_id=user_id,
+#                     age=age,
+#                     zipcode=zipcode)
+
+#         # We need to add to the session or it won't ever be stored
+#         db.session.add(user)
+
+#     # Once we're done, we should commit our work
+#     db.session.commit()
+
+
+def load_pill_data():
+    """Seed database with pillbox data. """
+
+    Meds.query.delete()
+
+    # '/home/vagrant/src/hb_pillproject/pill_testing_book.csv'
+
+    with open('pill_testing_book.csv') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        line_count = 0
+    # print(csv_reader)
+        for row in csv_reader:
+            # print(row)
+            if line_count == 0: 
+
+                print(f'This is the first row.')
+                print('#############')
+                line_count += 1 
+            else: 
+                print('THIS IS NOT THE FIRST ROW')
+                print('#############')
+                shape = row[9]
+                print(f'{shape} = shape',type(shape))
+
+                score = int(row[11])
+                print(f'{score} = score', type(score))
+
+                imprint = row[13]
+                print(f'{imprint} = imprint', type(imprint))
+
+                color = row[16]
+                print(f'{color} = color', type(color))
+
+                strength = row[18]
+                print(f'{strength} = strength', type(strength))
+
+                rxcui = row[24]
+                print(f'{rxcui} = rxcui', type(rxcui))
+
+                ndc9 = row[29]
+                print(f'{ndc9} = ndc9', type(ndc9))
+
+                medicine_name = row[32]
+                print(f'{medicine_name} = medicine_name',type(medicine_name))
+
+                image_label = row[45]
+                print(f'{image_label} = image_label', type(image_label))
+
+                has_image = row[46]
+                if has_image == 'TRUE':
+                    has_image = True 
+                else: 
+                    has_image = False
+
+                print(f'{has_image} = does this have an image',type(has_image))
+
+                #shape= row['splshape']
+
+
+                medication = Meds(shape=shape,
+                                  score=score,
+                                  imprint=imprint,
+                                  color=color,
+                                  strength=strength,
+                                  rxcui=rxcui,
+                                  ndc9=ndc9,
+                                  medicine_name=medicine_name,
+                                  image_label=image_label,
+                                  has_image=has_image)
+                db.session.add(medication)
+
+                # print(f'\t{row[32]} has been created with an rxcui of {row[24]}.')
+                
+                line_count += 1
+        # print(f'Processed {line_count} lines.')
+        db.session.commit()
+
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # In case tables haven't been created, create them
+    db.create_all()
+
+    # Import different types of data
+    load_pill_data()
+    
