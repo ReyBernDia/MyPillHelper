7f46d215cd7419effde9eb774daac0b4d3b8ce9b
Reyna Diaz <rb.diaz2015@gmail.com>
2019-12-01 13:34:56 -0800

    unit tests functioning.

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-# from server import app
+from server import app
 from model import db, connect_to_db #ALSO LIST NAME OF FUNCTIONS TO TEST
 import server 
 
@@ -11,22 +11,44 @@ class TestMyPillHelper(unittest.TestCase):
         self.client = server.app.test_client()
         server.app.config['TESTING'] = True
 
-    def homepage_rendering(self):
+    def test_homepage_rendering(self):
+        """Can we reach the homepage?"""
 
         result = self.client.get("/")
         self.assertIn(b"Pill Helper", result.data)
 
-    def search_bar_rendering(self):
-        pass
+    def test_find_meds_rendering(self):
+        """Are you able to enter in items to search for a pill."""
 
-    def search_results_rendering(self):
-        pass
+        result = self.client.get("/find_meds")
+        self.assertIn(b"Identify a Pill", result.data)
 
-    def more_info_rendering(self):
-        pass
+    def test_search_results_rendering(self):
+        """Do search results render after entering find-med info."""
 
-    def register_new_user(self):
-        pass
+        identify_info = {'pill_imprint': "APO", 'pill_shape': "Capsule"}
+
+        result = self.client.post("/results", data=identify_info,
+                                  follow_redirects=True)
+
+    def test_more_info_rendering(self):
+        """Can you see more information about selected medication."""
+
+        result = self.client.get("/more_info/Risperidone")
+        self.assertIn(b"More Information on Your Medication", result.data)
+
+
+    def test_register_page(self):
+        """Can you see the registration page."""
+
+        result = self.client.get("/register")
+        self.assertIn(b"Registration", result.data)
+
+    def test_render_login(self):
+        """Can see login page."""
+
+        result = self.client.get("/login")
+        self.assertIn(b"LOG IN", result.data)
 
 
 
