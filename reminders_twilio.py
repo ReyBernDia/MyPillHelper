aa2cc996500385d31584358518948577d123782d
Reyna Diaz <rb.diaz2015@gmail.com>
2019-12-03 13:42:35 -0800

    Homepage layout complete. Needs Font adjustments and coloring.

--- reminders_twilio.py
+++ reminders_twilio.py
@@ -226,8 +226,8 @@ if __name__ == "__main__":
     # As a convenience, if we run this module interactively, it will leave
     # you in a state of being able to work with the database directly.
 
-    # from server import app
-    # connect_to_db(app)
+    from server import app
+    connect_to_db(app)
     schedule.run_continuously(1)
 
 # if __name__ == "__server__":
